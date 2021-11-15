73b069f7c71af5296ad83d21bae7c391eca00a69
frchandra <nismara.chandra@gmail.com>
2021-11-15 09:49:26 +0700

    menambahkan fitur experimental

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -6,6 +6,8 @@ use App\Models\Dpt;
 use App\Models\Calon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Cache;
+use Prophecy\Call\Call;
 
 class DashboardController extends Controller{
     
@@ -15,13 +17,30 @@ class DashboardController extends Controller{
     }
 
     public function store(Request $request){
-        // var_dump($request->input('calonId'));
+        //increment perolehan suara
         $calonId = (int)$request->input('calonId');
         Calon::where('calon_id', $calonId)->update(['vote'=> DB::raw('vote+1')]);
+
+        //logout kan si user
         $request->session()->put('is_voted', false);
         $email = $request->session()->pull('email');
         $request->session()->flush();
+
+        //set user telah nyoblos 
         Dpt::where('email', $email)->update(['is_voted' => true]);
+
+        //optional, counter
+        // $out = Cache::get('visit');        
+        // if($out===null)
+        //     Cache::put('visit', 0);        
+        // if($out===10)
+        //     Cache::put('status',  function () {
+        //         //fetching             
+        //         return DB::table('dpts')->select(DB::raw('departemen, count(*)'))->where('is_voted', 1)->groupBy('departemen')->get();
+        //     });
+        //     Cache::decrement('visit', 10);                    
+        // Cache::increment('visit', 1);
+
         return redirect('/');
     }
 
--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,27 +8,25 @@ use Illuminate\Support\Facades\Cache;
 
 class HomeController extends Controller
 {
-    public function index(){
-              
-
-        // $out = Cache::remember('cache', 10, function () {
-        //     //fetching             
-        //     return DB::table('dpts')->select(DB::raw('departemen, count(*)'))->where('is_voted', 1)->groupBy('departemen')->get();
-        // });
+    public function index(){    
+        // $out = Cache::get('visit');        
+        // if($out===null)
+        //     Cache::put('visit', 0);        
+        // if($out===10)
+        //     Cache::decrement('visit', 10);  
+        // Cache::increment('visit', 1);
 
         // echo $out;
-
-        return view('welcome');
-        
+        return view('welcome');        
         // return \File::get(public_path() . '/index.html');
     }
 
     public function getStatus(){
-        $out = Cache::remember('cache', 10, function () {
+        $out = Cache::remember('status', 10, function () {
             //fetching             
             return DB::table('dpts')->select(DB::raw('departemen, count(*)'))->where('is_voted', 1)->groupBy('departemen')->get();
         });
-
+       
         return response()->json($out);
     }
 }
