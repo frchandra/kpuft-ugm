f2092ad24aa8641a6777bbffa9886b137933446d
frchandra <nismara.chandra@gmail.com>
2021-11-20 20:57:53 +0700

    debugging : CORS

--- config/cors.php
+++ config/cors.php
@@ -15,13 +15,13 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie', 'status/*'],
 
-    'allowed_methods' => ['*'],
+    'allowed_methods' => ['GET'],
 
     'allowed_origins' => ['*'],
 
-    'allowed_origins_patterns' => [],
+    'allowed_origins_patterns' => ['*'],
 
     'allowed_headers' => ['*'],
 
