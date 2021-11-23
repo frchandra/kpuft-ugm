bbbef7f8a9eee2a70df0d8a970d7b689d5908903
yosefadi <yosefadisulistyo@outlook.com>
2021-11-23 11:43:30 +0000

    Test

--- post_deploy.sh
+++ post_deploy.sh
@@ -1,19 +1,9 @@
 #!/bin/sh
 
-# optimize for deployment
-php artisan key:generate
-php artisan route:clear
-php artisan route:cache
-php artisan config:clear
-php artisan config:cache
-
 # update application cache
 php artisan optimize
 
-# set correct permission
-chown -R www-data:www-data /var/www
-chmod -R 755 /var/www
-
 # start the application
+
 php-fpm -D &&  nginx -g "daemon off;"
 
