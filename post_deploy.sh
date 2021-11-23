ba3a0d36e96c4bb233c43705968fb5d925fe9246
yosefadi <yosefadisulistyo@outlook.com>
2021-11-23 11:44:34 +0000

    Revert "Test"
    This reverts commit bbbef7f8a9eee2a70df0d8a970d7b689d5908903.

--- post_deploy.sh
+++ post_deploy.sh
@@ -1,9 +1,19 @@
 #!/bin/sh
 
+# optimize for deployment
+php artisan key:generate
+php artisan route:clear
+php artisan route:cache
+php artisan config:clear
+php artisan config:cache
+
 # update application cache
 php artisan optimize
 
-# start the application
+# set correct permission
+chown -R www-data:www-data /var/www
+chmod -R 755 /var/www
 
+# start the application
 php-fpm -D &&  nginx -g "daemon off;"
 
