873733932eb2b1e6b5c45c3226ca56ae593cf632
yosefadi <yosefadisulistyo@outlook.com>
2021-11-23 23:13:14 +0000

    Autorun OptiPNG on production deploy
    Signed-off-by: yosefadi <yosefadisulistyo@outlook.com>

--- post_deploy.sh
+++ post_deploy.sh
@@ -1,5 +1,7 @@
-#!/bin/sh
+#!/bin/bash
 
+if [ "$APP_DEBUG" != "true" ]; then
+echo "Optimizing App for Deployment Environment"
 # optimize for deployment
 php artisan key:generate
 php artisan route:clear
@@ -10,9 +12,14 @@ php artisan config:cache
 # update application cache
 php artisan optimize
 
+# Optimizing PNG file
+optipng -o1 /var/www/public/static/media/*
+
 # set correct permission
 chown -R www-data:www-data /var/www
 chmod -R 755 /var/www
+chmod 000 /var/www/public/phpinfo.php
+fi
 
 # start the application
 php-fpm -D &&  nginx -g "daemon off;"
